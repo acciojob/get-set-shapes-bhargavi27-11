e4bb1906925657d35797bf456a11c782ebea90b0
sesetty bhargavi <sesettybhargavi@gmail.com>
2026-03-18 17:30:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ class Rectangle {
 		return this._height;
 	}
 	getArea(){
-		return this.width * this.height;
+		console.log (this.width * this.height);
 	}
 }
 
@@ -20,7 +20,7 @@ class Square extends Rectangle {
 		super(side, side);
 	}
 	getPerimeter(){
-		return 4 * this.width;
+		console.log (4 * this.width);
 	}
 }
 
